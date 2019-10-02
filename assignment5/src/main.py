c85129d46eea5c4aad0de40bb1a21d473c933598
zyfhaha <707314696@qq.com>
2019-10-02 18:37:15 -0400

    code

--- assignment5/src/main.py
+++ assignment5/src/main.py
@@ -1,3 +1,4 @@
+import random
 from Map import Map
 from Back_tracking import Back_tracking
 from Back_forward import Back_forward
@@ -10,6 +11,7 @@ if pb=='usa':
     print('Which map do you want to search?(10/51):')
     num_state = int(input())
     map = Map('us_states_' + str(num_state) + '_ij.txt')
+    start_step=0
     print('Which algorithm do you want to use?(0/1/2/3)')
     print('0--pure back-tracking')
     print('1--back-tracking with forward-check')
@@ -17,6 +19,32 @@ if pb=='usa':
     print('3--Min-conflict search')
     pn=int(input())
     if pn==3:
-
-
-Min_conflict(map)
+        Min_conflict(map)
+        exit()
+    print('Which method do you want to use?(0/1/2):')
+    print('0--None')
+    print('1--Degree heuristic')
+    print('2-MRV')
+    s_type=int(input())
+    print('Do you want to use LCV?(Y/N)')
+    if input()=='Y':
+        lcv=1
+    else:
+        lcv=0
+    print('Do you want a initial assignment for one state?(Y/N)')
+    if input()=='Y':
+        map.colors[0]=random.randint(0, 3)
+        map.visited[0]=1
+        start_step=1
+    if pn==3:
+        Min_conflict(map)
+    if pn==0:
+        if num_state==51:
+            print('It will take a long time(about 5 minutes), do you want to wait(Y/N):')
+            if input()=='N':
+                exit()
+        Back_tracking(map,start_step,s_type,lcv)
+    if pn==1:
+        Back_forward(map,start_step,s_type,lcv)
+    if pn==2:
+        Back_AC3(map,start_step,s_type,lcv)
