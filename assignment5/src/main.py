e933f9e893d2cf4b2e82ddb55ee51a5e758e4b06
zyfhaha <707314696@qq.com>
2019-10-02 17:47:51 -0400

    code

--- assignment5/src/main.py
+++ assignment5/src/main.py
@@ -2,9 +2,11 @@ from Map import Map
 from Back_tracking import Back_tracking
 from Back_forward import Back_forward
 from Back_AC3 import Back_AC3
+from Min_conflict import Min_conflict
 
+print('Which problem do you want to solve?(usa or sudu)')
 print('Which map do you want to search?(10/51):')
 num_state=int(input())
 map = Map('us_states_'+str(num_state)+'_ij.txt')
 
-Back_AC3(map,0,2)
+Min_conflict(map)
